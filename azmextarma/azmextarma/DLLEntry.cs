bb5b1df083b0de40535a8232f3d3bf62b71cb257
alezm <31077802+alezm00@users.noreply.github.com>
2020-02-24 20:07:08 +0100

    asd

---
+++ azmextarma/azmextarma/DLLEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+class azmextarma
+{
+
+    #if WIN64
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+    #else
+            [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+    #endif
+    public static void RvExtensionVersion(StringBuilder output, int outputSize)
+    {
+        output.Append("Test-Extension v1.0");
+    }
+
+    #if WIN64
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+    #else
+            [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+    #endif
+    public static void RvExtension(StringBuilder output, int outputSize,
+        [MarshalAs(UnmanagedType.LPStr)] string function)
+    {
+        output.Append(function);
+    }
+}
