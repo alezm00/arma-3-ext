d5712d414511c0d107e6cf4d8c6d1f7144248017
alezm <31077802+alezm00@users.noreply.github.com>
2020-02-24 19:57:00 +0100

    aa

---
+++ firsttry/firsttry/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Maca134.Arma.DllExport;
+
+namespace firsttry
+{
+    public class firsttry
+    {
+        [ArmaDllExport]
+        public static string Invoke(string input, int maxsize)
+        {
+            return input;
+        }
+    }
+}
